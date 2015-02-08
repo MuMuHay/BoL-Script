3b792ff6d0f2726cc941d5b8bb4ac7631bf137c3
MuMuHay <MuMuHay@users.noreply.github.com>
2015-02-08 12:16:30 +0100

    Update The Void - Kassadin.lua

--- The Void - Kassadin.lua
+++ The Void - Kassadin.lua
@@ -1,11 +1,71 @@
--- Script rewrote from Nulled Kassadin by Hellsing.
--- Made by MuMuHey
  
  if myHero.charName ~= "Kassadin" then return end
 
+--[[
+ /$$$$$$$$ /$$                       /$$    /$$          /$$       /$$                     /$$   /$$                                              /$$ /$$          
+|__  $$__/| $$                      | $$   | $$         |__/      | $$                    | $$  /$$/                                             | $$|__/          
+   | $$   | $$$$$$$   /$$$$$$       | $$   | $$ /$$$$$$  /$$  /$$$$$$$                    | $$ /$$/   /$$$$$$   /$$$$$$$ /$$$$$$$  /$$$$$$   /$$$$$$$ /$$ /$$$$$$$ 
+   | $$   | $$__  $$ /$$__  $$      |  $$ / $$//$$__  $$| $$ /$$__  $$       /$$$$$$      | $$$$$/   |____  $$ /$$_____//$$_____/ |____  $$ /$$__  $$| $$| $$__  $$
+   | $$   | $$  \ $$| $$$$$$$$       \  $$ $$/| $$  \ $$| $$| $$  | $$      |______/      | $$  $$    /$$$$$$$|  $$$$$$|  $$$$$$   /$$$$$$$| $$  | $$| $$| $$  \ $$
+   | $$   | $$  | $$| $$_____/        \  $$$/ | $$  | $$| $$| $$  | $$                    | $$\  $$  /$$__  $$ \____  $$\____  $$ /$$__  $$| $$  | $$| $$| $$  | $$
+   | $$   | $$  | $$|  $$$$$$$         \  $/  |  $$$$$$/| $$|  $$$$$$$                    | $$ \  $$|  $$$$$$$ /$$$$$$$//$$$$$$$/|  $$$$$$$|  $$$$$$$| $$| $$  | $$
+   |__/   |__/  |__/ \_______/          \_/    \______/ |__/ \_______/                    |__/  \__/ \_______/|_______/|_______/  \_______/ \_______/|__/|__/  |__/
+                                                                                                                                                                   
+    
+	Credits: MuMuHey - Rewriting from Nulled Kassadin
+	Fantastik  helping , fixing and teaching me a lot
+	HeX teaching me a lot
+	
+	   Key   |                  What it does
+------------------------------------------------------------------------------------
+	Spacebar | Combo key - Uses Q, W and R. Spell usage can be disabled.
+------------------------------------------------------------------------------------
+		V    | Poke key - Uses Q to poke the enemy. Can be disabled.
+------------------------------------------------------------------------------------
+
+Other features:
+
+*		Everything can be disabled via the ingame menu.
+*		Orbwalking (move to mouse, cancel animation)
+*		Smart usage of combos
+*		Smart checking if a skill would kill an enemy instantly and casting that one
+*		Drawing ranges and combo range (when combo ready)
+*		Damage indicators
+*		Shows with how many hits the enemy could kill me
+*		Safe range mode if health is below 15%, means no R, only if R would kill him
+*		Automatic statpoints assignment
+*		More to come soon!
+
+changelog v 1.827
+	Fixing code and giving some awesomeness from Fantastik 
+
+changelog v 1.826
+	adding Autpupdate
+
+changelog v 1.825
+	adding Q harass
+	
+
+changelog v 1.222
+	improving some stuff
+
+changelog v 1.221
+	Changing some Code
+
+changelog v 1.120
+	Fixing Nulled Kassadin
+
+
+]]--
+
+
+
+
+
+
  
 --[[		Auto Update		]]
-local sversion = "1.826"
+local sversion = "1.827"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/MuMuHay/BoL-Script/master/The Void - Kassadin.lua".."?rand="..math.random(1,10000)

93ba30b78bdad7e90ff17a37ca7a2f83757e1e9e
MuMuHay <MuMuHay@users.noreply.github.com>
2015-02-08 11:53:11 +0100

    Update The Void - Kassadin.lua

--- The Void - Kassadin.lua
+++ The Void - Kassadin.lua
@@ -1,11 +1,36 @@
-local version = "1.001"
-
 -- Script rewrote from Nulled Kassadin by Hellsing.
 -- Made by MuMuHey
  
  if myHero.charName ~= "Kassadin" then return end
 
-  
+ 
+--[[		Auto Update		]]
+local sversion = "1.001"
+local AUTOUPDATE = true
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/MuMuHay/BoL-Script/master/The Void - Kassadin.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>The Void Kassadin:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTOUPDATE then
+	local ServerData = GetWebResult(UPDATE_HOST, "/MuMuHay/BoL-Script/master/version/The Void - Kassadin.version")
+	if ServerData then
+		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+		if ServerVersion then
+			if tonumber(sversion) < ServerVersion then
+				AutoupdaterMsg("New version available"..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..sversion.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
+end
+ 
 
 ------------ Globals ------------
 local player = myHero
